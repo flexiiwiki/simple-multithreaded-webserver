563d3e243f145c63d033a9f2fb43959ce603883b
flexiiwiki <100071255+flexiiwiki@users.noreply.github.com>
2023-06-01 03:42:24 -0600

    woops i had a number wrong

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let ip = &args[0];
-    let port: u16 = args[1].parse().unwrap();
-    let thread_nums: usize = args[2].parse().unwrap();
+    println!("{:#?}", args);
+
+    let ip = &args[1];
+    let port: u16 = args[2].parse::<u16>().unwrap();
+    let thread_nums: usize = args[3].parse::<usize>().unwrap();
 
     let config = ServerConfig::new(ip, port, thread_nums, true, None, None).unwrap();
 

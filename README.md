23d43deae14d6542545ee4e3d2eb8ef93c25e5c9
flexiiwiki <100071255+flexiiwiki@users.noreply.github.com>
2023-06-01 03:35:07 -0600

    Updated README, added basic CLI

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+
 # simple-multithreaded-webserver
 
 An expanded implementation of the final chapter of the rust book
+## Usage/Examples
+
+```console
+simple-multithreaded-webserver.exe (ip) (port) (amount of threads)
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use simple_multithreaded_webserver::ServerConfig;
+use std::env;
 
 fn main() {
-    let config = ServerConfig::new("192.168.0.48", 80, 5, true, None, None).unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    let ip = &args[0];
+    let port: u16 = args[1].parse().unwrap();
+    let thread_nums: usize = args[2].parse().unwrap();
+
+    let config = ServerConfig::new(ip, port, thread_nums, true, None, None).unwrap();
 
     config.start();
 }
